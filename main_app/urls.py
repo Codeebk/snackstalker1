cedf199a74731f3d0e53c3b407ad7b2f61eeacd5
omar <omasrt@gmail.com>
2019-06-20 16:31:28 -0500

    change home path

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.posts_index, name='home'),
   path('about/', views.about, name='about'),
   path('posts/', views.posts_index, name='index'),
   path('posts/<int:post_id>/', views.posts_detail, name='detail'),
