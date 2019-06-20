961ccdc5339190b1aff5b75ae43377f51d102c23
omar <omasrt@gmail.com>
2019-06-20 16:21:50 -0500

    add change username template

--- main_app/urls.py
+++ main_app/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
   path('acconts/signup', views.signup, name='signup'),
   path('posts/<int:post_id>/add_photo/', views.add_photo, name='add_photo'),
   path('profile/', views.get_user_profile, name='get_user_profile'),
-  path('accounts/password', views.change_password, name='change_password'),
-  
+  path('profile/change_password', views.change_password, name='change_password'),
+  path('profile/change_username', views.change_username, name='change_username'),
 ]
